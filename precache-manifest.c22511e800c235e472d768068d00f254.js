6033452a6f7804acb5b59319474da2fd7313634b
jnzhihuoo1 <593757535@qq.com>
2021-09-29 11:27:18 +0800

    update

---
+++ precache-manifest.c22511e800c235e472d768068d00f254.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "041767482ec7a414277db7f9896b9287",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99b275a5ca92acb4f5c1",
+    "url": "/static/css/2.929389fa.chunk.css"
+  },
+  {
+    "revision": "bbc8c373a62cf3f32fea",
+    "url": "/static/css/main.f3c791e8.chunk.css"
+  },
+  {
+    "revision": "99b275a5ca92acb4f5c1",
+    "url": "/static/js/2.f66f8ed4.chunk.js"
+  },
+  {
+    "revision": "ca9e56bc4f9848094e5b4e249ca2f146",
+    "url": "/static/js/2.f66f8ed4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbc8c373a62cf3f32fea",
+    "url": "/static/js/main.a63c0629.chunk.js"
+  },
+  {
+    "revision": "bda4a6a5a35ea013f007",
+    "url": "/static/js/runtime-main.f5679f09.js"
+  },
+  {
+    "revision": "4b8ec5cf962ffb622f6d7c0a1a8f8385",
+    "url": "/static/media/logo2.4b8ec5cf.png"
+  }
+]);
